b31aac33de47295362be29b3cb47cc39efb5b092
edusar <estudioedusar@hotmail.com>
2022-09-01 17:44:52 -0300

    Se modifico el navbar(incompleto)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+
 function App() {
   return (
     <div className="App">
-      <h1 className=" text-center bg-slate-500">Hola mundo</h1>
+  <h1 className=" text-center bg-slate-500">Hola mundo</h1>
+     
     </div>
   );
 }
---
+++ client/src/Vistas/Compartido/Componentes/Header/NavBar.js
@@ -0,0 +1,29 @@
+export default function NavBar (){
+
+
+
+    return (
+        <div className="flex items-center space-x-10 justify-center border-4 border-[10px] border-double">
+            <>Aqui va el logo</>
+            {/* <div className="flex items-center space-x-10 ">
+                Inicio
+                Conócenos
+                ¿Eres medico?
+                Blog
+            </div>
+          
+            <div>
+            
+                <select>
+                    <option>Registrarme</option>
+                    <option>Medico</option>
+                    <option>Usuario</option>
+                    <option>Admin</option>
+                </select>
+                <button>Iniciar Sesión</button>
+            </div> */}
+           
+        </div>
+    )
+
+}
