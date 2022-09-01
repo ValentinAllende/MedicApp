6e017351ce9298437a27b70b94cab3ed5e5fc747
GaRev-Dev <revelo.gabriel.38@gmail.com>
2022-09-01 18:09:58 -0500

    Actualizo y agrego rutas update, status y delete

--- api/src/controllers/controller.doctor.js
+++ api/src/controllers/controller.doctor.js
@@ -1,16 +1,22 @@
-const Doctor = require('../models/Doctor');
+const Doctor = require("../models/Doctor");
 
 const controllerDoctors = {
-    getAll: async (req, res, next) => {
-      try {
-        Doctor.find()
-        .then(doctors => res.status(200).send(doctors))
-      } catch (error) {
-        console.log(error);
+  getAll: async (req, res, next) => {
+    try {
+      const doctors = await Doctor.find();
+      if (!doctors) {
+        throwError(1301);
       }
-    },
-    createDoctor: async (req, res, next) => {
-      const { name, specialities, license, address, email, password, phoneNumber, hour, space, checkUpPrice} = req.body;
+      return res.status(200).send({ data: doctors });
+    } catch (error) {
+      return res.status(error.code || 500).send({ errors: error.message });
+    }
+  },
+  createDoctor: async (req, res, next) => {
+    const {
+      name, specialities, license, address, email, password, phoneNumber, hour, space, checkUpPrice,
+    } = req.body;
+    try {
       const newDoctor = new Doctor({
         name,
         specialities,
@@ -19,31 +25,100 @@ const controllerDoctors = {
         email,
         password,
         phoneNumber,
-        schedule:[
+        schedule: [
           {
             hour,
             space,
-            checkUpPrice
-          }
+            checkUpPrice,
+          },
         ],
         active: true,
       });
-      try {
-        newDoctor.save()
-          .then(doctor => res.status(200).send(doctor))
-      } catch (error) {
-        console.log(error);
+      await newDoctor.save();
+      return res.status(201).send({ newDoctor });
+    } catch (error) {
+      return res.status(error.code || 500).send({ errors: error.message });
+    }
+  },
+  getDoctor: async (req, res, next) => {
+    const { idDoctor } = req.params;
+    try {
+      const doctorById = await Doctor.findById(idDoctor);
+      if (!doctorById) {
+        throwError(1302);
       }
-    },
-    getDoctor: async (req, res, next) => {
-      const { idDoctor } = req.params;
-      try {
-        Doctor.find({_id: idDoctor})
-        .then(doctor => res.status(200).send(doctor))
-      } catch (error) {
-        console.log(error);
+      return res.status(200).send({ data: doctorById });
+    } catch (error) {
+      if (error.kind === "ObjectId") {
+        return res.status(403).send({ errors: "Formato de ID incorrecto" });
       }
-    },
-  };
-  
-  module.exports = controllerDoctors;
+      return res.status(error.code || 500).send({ errors: error.message });
+    }
+  },
+  updateDoctor: async (req, res, next) => {
+    const { idDoctor } = req.params;
+    const requestChanges = req.body; 
+    try {
+      const doctorForUpdate = await Doctor.findById(idDoctor);
+      if (!doctorForUpdate) {
+        throwError(1302);
+      }
+      Object.assign(doctorForUpdate, requestChanges);
+      await doctorForUpdate.save();
+      res.status(200).send({ data: doctorForUpdate });
+    } catch (error) {
+      if (error.kind === "ObjectId") {
+        return res.status(403).send({ errors: "Formato de ID incorrecto" });
+      }
+      return res.status(error.code || 500).send({ errors: error.message });
+    }
+  },
+  changeStatus: async (req, res, next) => {
+    const { idDoctor } = req.params;
+    try {
+      const doctor = await Doctor.findById(idDoctor);
+      if (!doctor) {
+        throwError(1302);
+      }
+      doctor.active = !doctor.active;
+      await doctor.save();
+      res.status(200).send({ data: doctor });
+    } catch (error) {
+      if (error.kind === "ObjectId") {
+        return res.status(403).send({ errors: "Formato de ID incorrecto" });
+      }
+      return res.status(error.code || 500).send({ errors: error.message });
+    }
+  },
+  deleteDoctor: async (req, res, next) => {
+    const { idDoctor } = req.params;
+    try {
+      const doctorForDelete = await Doctor.findById(idDoctor);
+      if (!doctorForDelete) {
+        throwError(1302);
+      }
+      await doctorForDelete.remove();
+      return res.status(200).send({ deleted: doctorForDelete });
+    } catch (error) {
+      if (error.kind === "ObjectId") {
+        return res.status(403).send({ errors: "Formato de ID incorrecto" });
+      }
+      return res.status(error.code || 500).send({ errors: error.message });
+    }
+  },
+};
+
+const throwError = (errorType) => {
+  if (errorType === 1301) {
+    const error = new Error("No se encontro ningun doctor");
+    error.code = 404;
+    throw error;
+  }
+  if (errorType === 1302) {
+    const error = new Error("El Doctor no existe");
+    error.code = 404;
+    throw error;
+  }
+};
+
+module.exports = controllerDoctors;
