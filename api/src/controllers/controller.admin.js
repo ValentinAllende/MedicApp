8dbcb5619e064a8cc32fdff3a76c9ed5b810415a
Esteban Velasquez <jestebanvelasquez22@gmail.com>
2022-09-01 19:25:52 -0500

    rutas & modelo  admin

---
+++ api/src/controllers/controller.admin.js
@@ -0,0 +1,67 @@
+const Admin = require("../models/Admin");
+
+const controllerAdmins = {
+    getAll: async (req, res, next) => {
+        try {
+            const admins = await Admin.find();
+            if (!admins.length) return res.status(400).json({ succes: false, message: "error 404" });
+            res.status(200).json({ succes: true, data: admins });
+        } catch (error) {
+            return res.status(400).json({ succes: false, message: error});
+        }
+    },
+    createAdmin: async (req, res, next) => {
+        const { name, email, password } = req.body;
+        try {
+            const adminBd= await Admin.exists({
+                email
+            })
+            if(adminBd) return res.status(400).json({ succes: false, message: " email ya registrado " })
+            const newAdmin = await Admin.create({
+                name,
+                email,
+                password,
+            });
+            res.status(200).json({succes: true, data: newAdmin})
+        } catch (error) {
+            return res.status(400).json({ succes: false, message: error});
+        }
+    },
+    getAdmin: async (req, res, next) => {
+        const { id } = req.params;
+        try {
+            try {
+                const admin = await Admin.findById(id)
+                if(!admin) return res.status(400).json({ succes: false, message: "error 404" })
+                res.status(200).json({succes: true, data: admin})
+                
+            } catch (error) {
+            return res.status(400).json({ succes: false, message: "error 404" });
+            }
+        } catch (error) {
+            return res.status(400).json({ succes: false, message: error});
+        }
+    },
+    pathAdmin: async (req, res, next) => {
+        const { id } = req.params;
+        const {enable} = req.body 
+        try {
+            try {
+                const admin = await Admin.findById(id)
+                if(!admin) return res.status(400).json({ succes: false, message: "error 404" })
+                const enableAdmin = await Admin.updateOne(
+                    {_id: id}, 
+                    {$set: {active:enable}}
+                    )
+                res.status(200).json({succes: true})
+                
+            } catch (error) {
+            return res.status(400).json({ succes: false, message: "error 404" });
+            }
+        } catch (error) {
+            return res.status(400).json({ succes: false, message: error});
+        }
+    },
+};
+
+module.exports = controllerAdmins;
--- api/src/models/Admin.js
+++ api/src/models/Admin.js
@@ -2,10 +2,27 @@ const mongoose = require( "mongoose");
 const {Schema} = require( "mongoose");
 
 const Admin = new Schema({
-    name:  String,
-    email: String,
-    password: String,
-    active: String,
+    name:  {
+        type: String,
+        required: true
+    },
+    image:  {
+        type: String,
+        // required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        default: true
+    },
     
 },
 {timestamps:true}
