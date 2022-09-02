08b5280fe9253fcfe4680eed039ef8cce07da4b3
SenDan07 <dany.alekrmz@gmail.com>
2022-09-01 22:45:14 -0700

    Componente de paginado creado

--- client/src/Vistas/Compartido/Componentes/Main/Listado/Pagination.js
+++ client/src/Vistas/Compartido/Componentes/Main/Listado/Pagination.js
@@ -1,1 +1,22 @@
 import React from "react";
+
+export default function Pagination({ resultsPerPage, totalResults, pages }) {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalResults / resultsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav className="pagination-block">
+      <ul className="pages-list">
+        {pageNumbers?.map((n) => (
+          <li className="page-item" key={n}>
+            <a href="#!" className="page-link" onClick={() => pages(n)}>
+              {n}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
