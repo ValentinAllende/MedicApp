fc4aee7255df9c700bc118af822f2b25b13a05c8
SenDan07 <dany.alekrmz@gmail.com>
2022-09-02 00:47:40 -0700

    Componente Paginado terminado

--- client/src/Vistas/Compartido/Componentes/Main/Listado/Pagination.js
+++ client/src/Vistas/Compartido/Componentes/Main/Listado/Pagination.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Pagination({ resultsPerPage, totalResults, pages }) {
+  const [currentPage, setCurrentPage] = useState(0);
+
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(totalResults / resultsPerPage); i++) {
+  const pageSum = Math.ceil(totalResults / resultsPerPage);
+
+  if (pageSum === 1) return null;
+  for (let i = 0; i <= pageSum; i++) {
     pageNumbers.push(i);
   }
 
   return (
-    <nav className="pagination-block">
+    <nav className="page-navigation">
       <ul className="pages-list">
+        {pageNumbers.includes(currentPage - 1) && (
+          <a
+            href="#!"
+            className="prev-page-link"
+            onClick={() => {
+              setCurrentPage(currentPage - 1);
+              pages(currentPage - 1);
+            }}
+          >
+            Previous
+          </a>
+        )}
         {pageNumbers?.map((n) => (
           <li className="page-item" key={n}>
             <a href="#!" className="page-link" onClick={() => pages(n)}>
@@ -16,6 +33,18 @@ export default function Pagination({ resultsPerPage, totalResults, pages }) {
             </a>
           </li>
         ))}
+        {pageNumbers.includes(currentPage + 1) && (
+          <a
+            className="next-page-link"
+            href="!#"
+            onClick={() => {
+              setCurrentPage(currentPage + 1);
+              pages(currentPage + 1);
+            }}
+          >
+            Next
+          </a>
+        )}
       </ul>
     </nav>
   );
