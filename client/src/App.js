a941ad027646e82d424b18abcf98752ce2e6a1be
edusar <estudioedusar@hotmail.com>
2022-09-04 20:05:23 -0300

    algunos cambios de estilos del searchbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Vistas/Compartido/Componentes/Home";
 import DetalleDoctor from "./Vistas/Compartido/Componentes/Main/DetalleDoctor";
 
+
 function App() {
   
   return (
+ 
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={ <Home/>} />
