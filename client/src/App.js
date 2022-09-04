7198f085e8b266005c4f5365f804934b1225b151
edusar <estudioedusar@hotmail.com>
2022-09-04 17:24:26 -0300

    algunos cambios de estilos del nav y el search

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,16 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Home from "./Vistas/Compartido/Componentes/Home";
-// import DetalleDoctor from "./Vistas/Compartido/Componentes/Main/DetalleDoctor";
-import SearchBar from "./Vistas/Compartido/Componentes/Header/SearchBar"
-import NavBar from "./Vistas/Compartido/Componentes/Header/NavBar"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Vistas/Compartido/Componentes/Home";
+import DetalleDoctor from "./Vistas/Compartido/Componentes/Main/DetalleDoctor";
 
 function App() {
   
   return (
-    <>
-    <NavBar/>
-    <SearchBar/>
-    </>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route exact path="/" element={ <Home/>} />
-    //     <Route exact path="/dummy/doctors/:idDoctor" element={ <DetalleDoctor/>} />
-    //   </Routes>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={ <Home/>} />
+        <Route exact path="/dummy/doctors/:idDoctor" element={ <DetalleDoctor/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
