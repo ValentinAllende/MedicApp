9d57ea8962cc16803b55dfbba52768ae920e5f0f
SenDan07 <dany.alekrmz@gmail.com>
2022-09-04 18:15:24 -0700

    Se arregla paginado

--- client/src/Vistas/Compartido/Componentes/Main/Listado/Pagination.js
+++ client/src/Vistas/Compartido/Componentes/Main/Listado/Pagination.js
@@ -1,56 +1,22 @@
 import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
 
-export default function Pagination({
-  badgesPerPage,
-  allBadges,
-  pages,
-  currentPage,
-}) {
-  // const [currentPage, setCurrentPage] = useState(1);
-
-  const pageNumbers = [];
-  const pageSum = Math.ceil(allBadges / badgesPerPage);
-
-  if (pageSum === 1) return null;
-  for (let i = 0; i <= pageSum; i++) {
-    pageNumbers.push(i);
+export default function Pagination({ badgesPerPage, allBadges, pages }) {
+  const pageNumber = [];
+  const pageBadgesCount = Math.ceil(allBadges / badgesPerPage);
+  for (let i = 1; i <= pageBadgesCount; i++) {
+    pageNumber.push(i);
   }
 
   return (
     <nav className="page-navigation">
       <ul className="pages-list">
-        {pageNumbers.includes(currentPage - 1) && (
-          <a
-            href="#!"
-            className="prev-page-link"
-            onClick={() => {
-              // setCurrentPage(currentPage - 1);
-              pages(currentPage - 1);
-            }}
-          >
-            Previous
-          </a>
-        )}
-        {pageNumbers?.map((n) => (
-          <li className="page-item" key={n}>
-            <a href="#!" className="page-link" onClick={() => pages(n)}>
-              {n}
+        {pageNumber?.map((number) => (
+          <li className="inline-block" key={number}>
+            <a href="#!" className="page-link" onClick={() => pages(number)}>
+              {number}
             </a>
           </li>
         ))}
-        {pageNumbers.includes(currentPage + 1) && (
-          <a
-            className="next-page-link"
-            href="!#"
-            onClick={() => {
-              // setCurrentPage(currentPage + 1);
-              pages(currentPage + 1);
-            }}
-          >
-            Next
-          </a>
-        )}
       </ul>
     </nav>
   );
