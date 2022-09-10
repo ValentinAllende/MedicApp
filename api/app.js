f2fb071c4db0264dace5034263eb3adcf51e7ea8
GaRev-Dev <revelo.gabriel.38@gmail.com>
2022-09-10 18:46:33 -0500

    pruebaDeploy

--- api/app.js
+++ api/app.js
@@ -1,49 +1,43 @@
 const dotenv = require("dotenv");
 dotenv.config();
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./src/routes/index');
-const  mongoose = require( "mongoose");
-const cors = require('cors')
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var indexRouter = require("./src/routes/index");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-mongoose.connect("mongodb+srv://esteban:12345@mediapp.yasrnqu.mongodb.net/test")
-.then(db => console.log("DB is connected"))
-.catch(error => console.log(error));
-require('./src/models/Admin')
+mongoose
+  .connect("mongodb+srv://esteban:12345@mediapp.yasrnqu.mongodb.net/test")
+  .then((res) => console.log("DB is connected"))
+  .catch((error) => console.log(error));
+require("./src/models/Admin");
 // var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(cors({origin:'*'}))
-app.use(logger('dev'));
+app.use(cors({ origin: "*" }));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
+  // Dominio que tengan acceso (ej. 'http://example.com')
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-    // Dominio que tengan acceso (ej. 'http://example.com')
-       res.setHeader('Access-Control-Allow-Origin', '*');
-    
-    // Metodos de solicitud que deseas permitir
-       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    
-    // Encabecedados que permites (ej. 'X-Requested-With,content-type')
-       res.setHeader('Access-Control-Allow-Headers', '*');
-    
-    next();
-    })
+  // Metodos de solicitud que deseas permitir
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 
+  // Encabecedados que permites (ej. 'X-Requested-With,content-type')
+  res.setHeader("Access-Control-Allow-Headers", "*");
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+  next();
+});
 
-// app.set('port', process.env.PORT || 3004)
+app.use("/", indexRouter);
 
-// app.listen(app.get('port'), () => {
-//   console.log(`Express server listening on port ${app.get('port')}`);
-// })
+app.get("/testDeploy", res.send("Funcionando"));
 
 module.exports = app;
