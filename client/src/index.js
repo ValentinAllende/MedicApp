befbb501c15bcc28f7606380176ba683da278fc8
GaRev-Dev <revelo.gabriel.38@gmail.com>
2022-09-10 19:39:39 -0500

    console.log

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,8 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3004";
 
+console.log("URL Backend: ", axios.defaults.baseURL)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
