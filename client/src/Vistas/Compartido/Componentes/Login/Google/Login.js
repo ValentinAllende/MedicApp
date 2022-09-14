2ec8493f144517428aad275bddec71490fcea476
GaRev-Dev <revelo.gabriel.38@gmail.com>
2022-09-14 17:52:01 -0500

    Estilos form ingreso

--- client/src/Vistas/Compartido/Componentes/Login/Google/Login.js
+++ client/src/Vistas/Compartido/Componentes/Login/Google/Login.js
@@ -1,64 +1,70 @@
-import React, { useEffect } from 'react'
-import GoogleLogin from 'react-google-login';
-import { gapi } from 'gapi-script'
-import axios from 'axios'
+import React, { useEffect } from "react";
+import GoogleLogin from "react-google-login";
+import { gapi } from "gapi-script";
+import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 
+export default function Google({ login, className }) {
+  const navigate = useNavigate();
+  const clientId =
+    "31532081050-nrgri514im6srt8c3e4thvg2dg6ionem.apps.googleusercontent.com";
+  useEffect(() => {
+    gapi.load("client:auth2", () => {
+      gapi.auth2.init({ clientId: clientId });
+    });
+  }, []);
 
+  const responseGoogle = async (response) => {
+    console.log(response);
 
-export default function Google(props) {
-    const navigate = useNavigate()
-    const clientId = "31532081050-nrgri514im6srt8c3e4thvg2dg6ionem.apps.googleusercontent.com";
-    useEffect(() => {
-        gapi.load("client:auth2", () => {
-            gapi.auth2.init({ clientId: clientId })
-        })
-    }, [])
+    const respuesta = await axios({
+      url: "/auth/google",
+      method: "POST",
+      data: { google: response.tokenId },
+    });
+    console.log(respuesta.data.token, "token");
+    console.log(respuesta.data.data, "data");
+    localStorage.setItem("auth-token", JSON.stringify(respuesta.data.token));
+    localStorage.setItem("User", JSON.stringify(respuesta.data.data));
+    window.sessionStorage.setItem(
+      "Rol",
+      JSON.stringify(respuesta.data.data.rol)
+    );
+    window.sessionStorage.setItem("isAuth", true);
+    login();
+    // if (respuesta.data.data.rol === 'PATIENT') {
+    //         navigate('/')
+    //     }
+  };
 
-    const responseGoogle = async (response) => {
+  const customStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    justifyContent: "space-evenly",
+    gap: '10px'
+  };
 
-        
-        console.log(response)
-
-        const respuesta = await axios({
-            url: '/auth/google',
-            method: 'POST',
-            data: {google:response.tokenId},
-            
-
-        })
-        console.log(respuesta.data.token, "token")
-        console.log(respuesta.data.data, "data")
-        localStorage.setItem("auth-token", JSON.stringify(respuesta.data.token));
-        localStorage.setItem("User", JSON.stringify(respuesta.data.data));
-        window.sessionStorage.setItem(
-          "Rol",
-          JSON.stringify(respuesta.data.data.rol)
-        );
-        window.sessionStorage.setItem("isAuth", true);
-
-        props.login()
-        // if (respuesta.data.data.rol === 'PATIENT') {
-        //         navigate('/')
-        //     }
-
-            
-    }   
-
-    
-
-
-
-
-    return (
-        <div>
-            <GoogleLogin
-                clientId={clientId}
-                buttonText="Ingresar con Google"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-                cookiePolicy={'single_host_origin'}
-            />
-        </div>
-    )
+  return (
+    <GoogleLogin
+      clientId={clientId}
+      buttonText="Ingresar con Google"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={"single_host_origin"}
+      render={(renderProps) => (
+        <button onClick={renderProps.onClick} style={customStyle}>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/2991/2991148.png"
+            style={{
+              width: "20px",
+              height: "20px",
+            }}
+            alt="google"
+          />
+          Ingresa con Google
+        </button>
+      )}
+    />
+  );
 }
