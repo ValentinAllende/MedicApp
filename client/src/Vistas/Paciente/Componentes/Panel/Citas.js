4372b0b5107d1efb2fac4b541006d38fb709f07f
mariapaulav <mariapaulaviviz@gmail.com>
2022-09-14 16:16:00 -0500

    panel paciente ajuste diseno

--- client/src/Vistas/Paciente/Componentes/Panel/Citas.js
+++ client/src/Vistas/Paciente/Componentes/Panel/Citas.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Citas = () => {
+let dateJs
+let date2 = dateJs.toISOString()
+console.log(date2);
+
+let citaPAg = '2022-09-07T00:00:00.000Z'
+
     return (
         <div>
-            Aca apareceran tus citas
+            <p>Tus citas pendientes</p>
+            <p>Tus citas terminadas: 
+                {dateJs}
+            </p>
+            <p>{citaPAg}</p>
         </div>
     );
 }
