96cacb15f01a121b7ed59aaa1c1031e42cc34d1c
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-09-15 18:13:01 -0500

    Responsive (Mobile) Admin Panel

--- client/src/Vistas/Admin/componentes/Dashboard/TopBar/TopBar.jsx
+++ client/src/Vistas/Admin/componentes/Dashboard/TopBar/TopBar.jsx
@@ -1,27 +1,29 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./TopBar.module.css";
 import avatar from "../../../assets/dashboard/avatar-test.jpeg";
 import menu from "../../../assets/dashboard/menu-icon.svg";
 import Banner from "../../Dashboard/Banner/Banner";
-import { useEffect } from "react";
+import { changeSectionDashboard } from "../../../../../Redux/actions/generalActionsAdmins";
 import { LOGOUT_ADMIN } from "../../../../../context/config/routes/paths";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 const TopBar = () => {
-
   const [toggle, setToggle] = useState(false);
-  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const changeItemMenu = (section) => {
+    dispatch(changeSectionDashboard(section));
+  };
 
   const onClickMenu = () => {
     setToggle(!toggle);
   };
 
-  const dataAdmin = JSON.parse(window.localStorage.getItem('User'));
+  const dataAdmin = JSON.parse(window.localStorage.getItem("User"));
 
-  useEffect(()=>{
-    console.log(dataAdmin);
-
-  },[])
+  const sizeWindow = window.screen.width;
 
   return (
     <>
@@ -33,17 +35,56 @@ const TopBar = () => {
         <div className={styles.Avatar}>
           <img src={avatar} alt="avatar-admin" />
         </div>
-        <div className={styles.Dropdown} >
-          <img src={menu} alt="menu-icon" className={styles.IconMenu} onClick={(e) => onClickMenu(e)} />
-          <ul className={toggle ?`${styles.Menu} ${styles.MenuOpen}`: styles.Menu}>
-            <li><Link to="/admin/home">Inicio</Link></li>
-            <li>Configuración</li>
-            <Link to={LOGOUT_ADMIN}>Cerrar sesion</Link>
+        <div className={styles.Dropdown}>
+          <img
+            src={menu}
+            alt="menu-icon"
+            className={styles.IconMenu}
+            onClick={(e) => onClickMenu(e)}
+          />
+          <ul
+            className={
+              toggle ? `${styles.Menu} ${styles.MenuOpen}` : styles.Menu
+            }
+          >
+            <li>
+              <Link to="/admin/home">Inicio</Link>
+            </li>
+
+            {sizeWindow < 700 && (
+              <>
+                <li>
+                  <button onClick={() => changeItemMenu("Dashboard")}>
+                    Dashboard
+                  </button>
+                </li>
+                <li>
+                  <button onClick={() => changeItemMenu("Pacientes")}>
+                    Pacientes
+                  </button>
+                </li>
+                <li>
+                  <button onClick={() => changeItemMenu("Doctores")}>
+                    Doctores
+                  </button>
+                </li>
+                <li>
+                  <button onClick={() => changeItemMenu("Admins")}>
+                    Admins
+                  </button>
+                </li>
+                <li>
+                  <button onClick={() => changeItemMenu("Citas")}>Citas</button>
+                </li>
+              </>
+            )}
+            <li>
+              <Link to={LOGOUT_ADMIN}>Cerrar sesion</Link>
+            </li>
           </ul>
         </div>
-        
       </section>
-     {/*  <Banner/> */}
+      {/*  <Banner/> */}
     </>
   );
 };
