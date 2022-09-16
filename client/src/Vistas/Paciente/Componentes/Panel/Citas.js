d36d21e206145e7b3139e608fdfd1caa17ea33fa
mariapaulav <mariapaulaviviz@gmail.com>
2022-09-16 11:53:15 -0500

    panel paciente ok / pendiente incluir nombre doctor en cita y hacerlo responsive

--- client/src/Vistas/Paciente/Componentes/Panel/Citas.js
+++ client/src/Vistas/Paciente/Componentes/Panel/Citas.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import {getPatientToken } from '../../../../Redux/actions/generalActionsPatients';
 import Home from '../../../Compartido/Componentes/Home';
 import FormResena from './formResena';
-import Resenas from './Resenas';
+
 
 const Citas = ({Resenas}) => {
     const dispatch = useDispatch();
@@ -20,7 +20,7 @@ const Citas = ({Resenas}) => {
 
     let appointments = profile.appointment
     console.log(appointments ,' los appoints');
-
+   
     const hoy2 = new Date(Date.now()).toISOString().split('T')[0]
 
 
@@ -31,7 +31,7 @@ const Citas = ({Resenas}) => {
                 return (
                     <>
 
-                    <div className='m-2'>
+                    <div className='m-2 bg-white rounded'>
                         <span className="text-[#292F53] text-lg font-poppins  mt-3 mb-4">{cita.date.split('T')[0] > hoy2 &&  
                         <span>Fecha: </span>}</span>
                         <span className="text-[#292F53] text-lg font-raleway  mt-3 mb-4">{cita.date.split('T')[0] > hoy2 &&  cita.date.split('T')[0]}</span>
@@ -50,8 +50,7 @@ const Citas = ({Resenas}) => {
             {appointments && appointments?.map( cita => {
                 return (
                     <>
-
-                    <div className='m-2'>
+                    <div className='m-2 bg-white rounded'>
                         <span className="text-[#292F53] text-lg font-poppins  mt-3 mb-4">{cita.date.split('T')[0] < hoy2 &&  
                         <span>Fecha: </span>}</span>
                         <span className="text-[#292F53] text-lg font-raleway  mt-3 mb-4">{cita.date.split('T')[0] < hoy2 &&  cita.date.split('T')[0]}</span>
@@ -60,21 +59,21 @@ const Citas = ({Resenas}) => {
                         <span className="text-[#292F53] text-lg font-poppins  mt-3 mb-4">Hora: </span>}</span>
                         <span className="text-[#292F53] text-lg font-raleway  mt-3 mb-4">{cita.date.split('T')[0] < hoy2 && cita.hour}</span>
 
-                        <div>{cita.date.split('T')[0] < hoy2 && 
-                            <button className='bg-[#1479FF] rounded p-2 font-poppins text-white tracking-wide' value = {cita._id} onClick={ (e) =>  
-                                {
-                                setOpenForm(true)
-                                console.log(e.target.value, 'el id de la cita');
-                                }
-                            }>Deja tu reseña</button> 
-                        }
-                            {cita.date.split('T')[0] < hoy2 && openForm && <FormResena 
-                            idCita = {cita._id}
-                            />}
+                        <div>
+                            {cita.date.split('T')[0] < hoy2 && cita.comment.length < 10 &&
+                            <button className='bg-[#1479FF] rounded p-2 font-poppins text-white tracking-wide m-2' value = {cita._id} onClick={ (e) =>{setOpenForm(true)}}
+                            >Deja tu reseña</button> 
+                            }
 
+                            {cita.date.split('T')[0] < hoy2 && cita.comment.length > 4 &&
+                            <p className="text-[#292F53] text-lg font-raleway  mt-2 mb-4" >{cita.date.split('T')[0] < hoy2 && cita.comment}</p> 
+                            }
+
+                            {cita.date.split('T')[0] < hoy2 && openForm && cita.comment.length < 5 && 
+                            <FormResena idCita = {cita._id}/>
+                            }
                         </div>
                     </div>
-                    
                     </>
                 )
             })}
