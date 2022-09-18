1386296470753522b5da1d9ee773dcacecb52f53
GaRev-Dev <revelo.gabriel.38@gmail.com>
2022-09-17 21:47:50 -0500

    Cambio estilos Modal

--- client/src/Vistas/Compartido/Componentes/SwalStyled/index.js
+++ client/src/Vistas/Compartido/Componentes/SwalStyled/index.js
@@ -2,8 +2,7 @@ import Swal from "sweetalert2";
 
 const Modal = Swal.mixin({
   customClass: {
-    popup: "rounded-lg p-10",
-    title: "font-poppins p-0",
+    popup: "rounded-lg p-5 pb-10 font-poppins",
     confirmButton:
       "bg-[#292F53] hover:bg-[#1479FF] py-4 px-8 rounded-lg border-0 text-white font-poppins",
   },
